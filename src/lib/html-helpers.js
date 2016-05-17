f77db67f5e9a797a3adab272428f75fee4730bb3
Chris Dosé <chris@peek.com>
2016-05-17 09:24:36 -0700

    Remove linking for now, until I can fix it properly.

--- src/lib/html-helpers.js
+++ src/lib/html-helpers.js
@@ -1,8 +1,8 @@
 import ansiUp from 'ansi_up';
-const { ansi_to_html: ansiToHtml, escape_for_html: escapeForHtml, linkify } = ansiUp;
+const { ansi_to_html: ansiToHtml, escape_for_html: escapeForHtml } = ansiUp;
 
 export function colorize(str) {
-  return linkify(ansiToHtml(str, { use_classes: true }));
+  return ansiToHtml(str, { use_classes: true });
 }
 
 export function escapeHTML(str) {
