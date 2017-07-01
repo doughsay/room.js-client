1147f1c1cab2f576c7399236266cfccbdb2fd509
Chris Dosé <chris@peek.com>
2017-06-30 18:26:09 -0700

    Refactor so we can test search results.

--- src/view-models/search-view-model.js
+++ src/view-models/search-view-model.js
@@ -1,4 +1,5 @@
 import ko from 'knockout'
+import SearchResult from '../lib/search-result'
 
 function isElementVisibleIn (el, container) {
   const elRect = el.getBoundingClientRect()
@@ -10,59 +11,6 @@ function isElementVisibleIn (el, container) {
     elRect.bottom <= containerRect.bottom &&
     elRect.right <= containerRect.right
   )
-}
-
-class SearchResult {
-  constructor (data) {
-    this.data = data
-    this.objectId = data.objectId
-    this.active = ko.observable(false)
-    this.name = ko.computed(this.computeName.bind(this))
-  }
-
-  static newFromResult (result) {
-    if (result.function) {
-      return new FunctionSearchResult(result)
-    } else if (result.verb) {
-      return new VerbSearchResult(result)
-    }
-
-    return null
-  }
-
-  computeName () {
-    throw new Error('Must be subclassed.')
-  }
-
-  openEditor () {
-    throw new Error('Must be subclassed.')
-  }
-}
-
-class FunctionSearchResult extends SearchResult {
-  computeName () {
-    return `${this.objectId}.${this.data.function}`
-  }
-
-  openEditor (socket, tabsViewModel) {
-    const params = { objectId: this.objectId, name: this.data.function }
-    socket.emit('get-function', params, data => {
-      tabsViewModel.newEditFunctionTab(socket, data)
-    })
-  }
-}
-
-class VerbSearchResult extends SearchResult {
-  computeName () {
-    return `${this.objectId}.${this.data.verb}`
-  }
-
-  openEditor (socket, tabsViewModel) {
-    const params = { objectId: this.objectId, name: this.data.verb }
-    socket.emit('get-verb', params, data => {
-      tabsViewModel.newEditVerbTab(socket, data)
-    })
-  }
 }
 
 // TODO: socket permissions
